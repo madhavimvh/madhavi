0242b89dc36ffbba3196401157405f148b075ea8
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-04 10:21:53 +0530

    " -> 0 of 7 passed. pylint: 0.91/10 "

---
+++ cspp1-assignments/p1 (2)/p1/mod6ass1exam.py
@@ -0,0 +1,12 @@
+def main():
+    n = int(input("number"))
+    for i in range (n+1):
+        if (i%3==0 and i%5==0):
+            print("FizzBuzz")
+        elif (i%3==0):
+            print("Fizz")
+        elif (i%5==0):
+            print("Buzz")
+        else:
+            print(i)
+main()
