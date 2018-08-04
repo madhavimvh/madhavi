a29b337dcdbaf8cb7343a0060fcb5048152c2f75
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-04 10:33:40 +0530

    " -> 7 of 7 passed. pylint: 1.67/10 "

--- cspp1-assignments/p1 (2)/p1/mod6ass1exam.py
+++ cspp1-assignments/p1 (2)/p1/mod6ass1exam.py
@@ -1,7 +1,7 @@
 """program to print nos from 1 to N"""
 
 def main():
-    n = int(input("number"))
+    n = int(input())
     for i in range (1,n+1):
         if (i%3==0 and i%5==0):
             print("Fizz")
