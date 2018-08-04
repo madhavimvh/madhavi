e268f8d59aac1cd233375707593a095870d633b2
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-04 12:59:16 +0530

    " -> 7 of 7 passed. pylint: -4.44/10 "

--- cspp1-assignments/m6/p2/special_char1.py
+++ cspp1-assignments/m6/p2/special_char1.py
@@ -7,7 +7,7 @@ def main():
     s = str(input())
     z = ""
     for n in s:
-        if n =="!" or n =="@" or n =="#" or n =="$" or n =="%" or n =="^" or n =="&" or n =="*":
+        if n == "!" or n == "@" or n == "#" or n == "$" or n == "%" or n == "^" or n == "&" or n == "*":
              z+=" "
         else:
             z+=n
