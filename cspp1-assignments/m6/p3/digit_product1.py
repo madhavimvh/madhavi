dd06a72a3d8b78ba09db6c9ef1fc67ea58fc4785
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-04 11:11:55 +0530

    " -> 2 of 7 passed. pylint: -0.83/10 "

--- cspp1-assignments/m6/p3/digit_product1.py
+++ cspp1-assignments/m6/p3/digit_product1.py
@@ -2,11 +2,13 @@ def main():
     n = int(input())
     p=1
     i=1
+    if n==0:
+        print("0")
     while n>0:
         rem = n%10
         p = p*rem
         n = n//10
-    print(p)
+        print(p)
 main()
         
         
