39e03892a9256b1331aecf976c8cea71b06753d9
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-09 17:00:00 +0530

    biggest

---
+++ cspp1-practice/m10/biggest Exercise/biggest_exercise.py
@@ -0,0 +1,34 @@
+#Exercise : Biggest Exercise
+#Write a procedure, called biggest, which returns the key corresponding to the entry with the largest number of values associated with it. If there is more than one such entry, return any one of the matching keys.
+
+
+def biggest(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+
+    returns: The key with the largest number of values associated with it
+    '''
+    maxm = 0
+    for word in aDict:
+        m = len(aDict[word])
+        if maxm < m:
+            maxm = m
+            temp = word
+    return temp
+     
+    
+
+def main():
+    n=input()
+    aDict={}
+    for i in range(int(n)):
+    	s=input()
+    	l=s.split()
+    	if l[0][0] not in aDict:
+    		aDict[l[0][0]]=[l[1]]
+    	else:
+    		aDict[l[0][0]].append(l[1])
+    print(biggest(aDict))
+
+if __name__== "__main__":
+    main()
