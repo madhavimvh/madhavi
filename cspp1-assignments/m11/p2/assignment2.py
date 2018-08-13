9f450906e8655bb2e8537d2be7a350b7fb86625d
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-13 14:18:05 +0530

    " -> 6 of 6 passed. pylint: -6.25/10 "

--- cspp1-assignments/m11/p2/assignment2.py
+++ cspp1-assignments/m11/p2/assignment2.py
@@ -1,33 +1,27 @@
 """program to update hand"""
 #Exercise: Assignment-2
-#Implement the updateHand function. Make sure this function has no side effects: i.e., it must not mutate the hand passed in. Before pasting your function definition here, be sure you've passed the appropriate tests in test_ps4a.py.
-
-
+#Implement the updateHand function.
+#Make sure this function has no side effects: i.e.,
+#it must not mutate the hand passed in. Before pasting
+#your function definition here, be sure you've passed the
+#appropriate tests in test_ps4a.py.
 def update_hand(hand, word):
     """
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
     a letter appears in 'word', 'hand' has at least as
     many of that letter in it. 
-
     Updates the hand: uses up the letters in the given word
     and returns the new hand, without those letters in it.
-
     Has no side effects: does not modify hand.
-
     word: string
     hand: dictionary (string -> int)    
     returns: dictionary (string -> int)
-    """
-    
+    """   
     for letter in word:
         if letter in hand:
             hand[letter]=hand[letter] - 1
     return hand
-            
-        
-    
-
 def main():
 	n=input()
 	adict={}
@@ -37,8 +31,5 @@ def main():
 		adict[l[0]]=int(l[1])
 	data1=input()
 	print(update_hand(adict,data1))
-		
-
-
 if __name__== "__main__":
 	main()
