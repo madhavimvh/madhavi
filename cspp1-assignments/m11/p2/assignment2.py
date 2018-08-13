b716a972d564be8ce44542325a754a48c4b44ce4
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-13 20:57:19 +0530

    " -> 6 of 6 passed. pylint: 6.88/10 "

--- cspp1-assignments/m11/p2/assignment2.py
+++ cspp1-assignments/m11/p2/assignment2.py
@@ -15,7 +15,7 @@ def update_hand(hand, word):
     and returns the new hand, without those letters in it.
     Has no side effects: does not modify hand.
     word: string
-    hand: dictionary (string -> int) 
+    hand: dictionary (string -> int)
     returns: dictionary (string -> int)
     """
     for letter in word:
@@ -26,10 +26,10 @@ def main():
     n = input()
     adict = {}
     for i in range(int(n)):
-    	data = input()
-    	l = data.split()
-    	adict[l[0]] = int(l[1])
+        data = input()
+        l = data.split()
+        adict[l[0]] = int(l[1])
     data1 = input()
     print(update_hand(adict, data1))
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+        main()
