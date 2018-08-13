025486324cd526a48cf49ac469ba8ee63f2739fe
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-13 14:20:23 +0530

    " -> 0 of 6 passed. pylint: 0/0 "

--- cspp1-assignments/m11/p2/assignment2.py
+++ cspp1-assignments/m11/p2/assignment2.py
@@ -10,26 +10,26 @@ def update_hand(hand, word):
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
     a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
+    many of that letter in it.
     Updates the hand: uses up the letters in the given word
     and returns the new hand, without those letters in it.
     Has no side effects: does not modify hand.
     word: string
-    hand: dictionary (string -> int)    
+    hand: dictionary (string -> int) 
     returns: dictionary (string -> int)
-    """   
+    """
     for letter in word:
         if letter in hand:
-            hand[letter]=hand[letter] - 1
+            hand[letter] = hand[letter] - 1
     return hand
 def main():
-	n=input()
-	adict={}
-	for i in range(int(n)):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	data1=input()
-	print(update_hand(adict,data1))
+    n = input()
+    adict = {}
+    for i in range(int(n)):
+    	data = input()
+	l = data.split()
+	adict[l[0]] = int(l[1])
+    data1 = input()
+    print(update_hand(adict,data1))
 if __name__== "__main__":
 	main()
