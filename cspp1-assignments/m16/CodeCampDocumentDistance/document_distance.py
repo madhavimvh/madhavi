b625edeceff408cf47d254f049e63358ac769e6b
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-17 22:37:33 +0530

    " -> 3 of 3 passed. pylint: 7.94/10 "

--- cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -42,17 +42,17 @@ def frequency_list(words_list1, words_list2):
     freq_list = {}
     for each_word in words_list1:
         if len(each_word) > 0:
-        if each_word not in freq_list:
-            freq_list[each_word] = [1, 0]
-        else:
-            freq_list[each_word][0] += 1
+            if each_word not in freq_list:
+                freq_list[each_word] = [1, 0]
+            else:
+                freq_list[each_word][0] += 1
     
     for each_word in words_list2:
         if len(each_word) > 0:
-        if each_word not in freq_list:
-            freq_list[each_word] = [0, 1]
-        else:
-            freq_list[each_word][1] += 1
+            if each_word not in freq_list:
+                freq_list[each_word] = [0, 1]
+            else:
+                freq_list[each_word][1] += 1
     #print(freq_list)
     return freq_list
 
