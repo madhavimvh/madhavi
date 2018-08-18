454538b79b10856557593616f989218cbfe11ef8
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-18 12:31:54 +0530

    " -> 0 of 1 passed. pylint: 9.25/10 "

--- cspp1-assignments/m18/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/build_search_index.py
@@ -79,18 +79,18 @@ def build_search_index(docs):
     words_list = word_list(docs)
     #print("gh",words_list)
     docs = clean_up(words_list)
-    print("dp1",docs)
+    #print("dp1",docs)
     search1_index = {}
     i = 0
     while i < len(docs):
         for each_word in docs:
-            print("do",docs)
+            #print("do",docs)
             if each_word not in search1_index:
                 search1_index[each_word] = [1, 0]
-                print("1",search1_index)
+                #print("1",search1_index)
             else:
                 search1_index[each_word][i][0] += 1
-                print("2",search1_index)
+                #print("2",search1_index)
 
     while i < len(docs):
         for each_word in docs:
