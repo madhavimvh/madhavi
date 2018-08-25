270659257a9b308d463f3297029adb8b6563bc3b
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-25 11:18:38 +0530

    " -> 0 of 4 passed. pylint: 7.14/10 "

--- cspp1-assignments/m22/assignment3/tokenize.py
+++ cspp1-assignments/m22/assignment3/tokenize.py
@@ -2,12 +2,30 @@
 Write a function to tokenize a given string and return a dictionary with the frequency of
 each word
 '''
-
+import re
 def tokenize(string):
-    pass
+    string = string.lower().split(" ")
+    string_list = []
+    string_dic = {}
+    for each in range(len(string)-1):
+        string_list.append(re.sub("[^a-z]", "", string[each]))
+    for word in string_list:
+        if word not in string_dic:
+            string_dic[word] = string.count(word)
+    return string_dic
+    
+    
             
 def main():
-    pass
+    lines = int(input())
+    string = ""
+    for i in range(lines):
+        string += input()
+        string += '\n'
+        i += 1
+    #print(string)
+    print(tokenize(string))
+        
 
 if __name__ == '__main__':
     main()
