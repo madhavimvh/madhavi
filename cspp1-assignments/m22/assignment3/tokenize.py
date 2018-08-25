77e2681a752ab899cfbd092b97b71f8d1458768a
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-25 13:06:20 +0530

    " -> 3 of 4 passed. pylint: 7.20/10 "

--- cspp1-assignments/m22/assignment3/tokenize.py
+++ cspp1-assignments/m22/assignment3/tokenize.py
@@ -5,19 +5,19 @@ each word
 import re
 def tokenize(string):
     string_1 = string.split(" ")
-    print("s",string_1)
+    #print("s",string_1)
     string_list = []
     string_dic = {}
     for each in range(len(string_1)):
         string_list.append(re.sub("[^a-z,A-Z,0-9]", "", string_1[each]))
-    print("ss",string_list)
+    #print("ss",string_list)
     for word in string_list:
         if word not in string_dic:
             string_dic[word] = 1
-            print("d",string_dic)
+            #print("d",string_dic)
         else:
             string_dic[word] += 1
-    print("dd",string_dic)
+    #print("dd",string_dic)
     return string_dic
     
     
@@ -32,7 +32,7 @@ def main():
     for each in string:
         if each == '\n':
             string.replace(each, "")
-    print(string)
+    #print(string)
     print(tokenize(string))
         
 
