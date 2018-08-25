873754ae365b445ce073b6c54570b7b0c77ac36a
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-25 10:38:22 +0530

    " -> 2 of 2 passed. pylint: 4.44/10 "

---
+++ cspp1-assignments/m22/assignment1/read_input.py
@@ -0,0 +1,17 @@
+'''
+Write a python program to read multiple lines of text input and store the input into a string.
+'''
+
+def main():
+    lines_n = input()
+    string_n = ""
+    for i in range(int(lines_n)):
+        string_n += input()
+        string_n += '\n'
+        
+    print(string_n)
+        
+    
+
+if __name__ == '__main__':
+    main()
