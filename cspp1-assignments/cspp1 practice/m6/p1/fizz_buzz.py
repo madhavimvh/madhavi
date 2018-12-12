b858a1e351a99faa7ea7b9d87aa57b3126c2a09b
madhavimvh <mvhmadhavi@msitprogram.net>
2018-12-12 19:08:55 +0530

    " -> 2 of 7 passed. pylint: 3.33/10 "

--- cspp1-assignments/cspp1 practice/m6/p1/fizz_buzz.py
+++ cspp1-assignments/cspp1 practice/m6/p1/fizz_buzz.py
@@ -12,9 +12,9 @@ def main():
     for i in range(num):
 	    if num % 3 and num % 5 == 0:
 	    	print("FizzBuzz")
-	    elif num % 3 == 0:
+	    if num % 3 == 0:
 	    	print("Fizz")
-	    elif num % 5 == 0:
+	    if num % 5 == 0:
 	    	print("Buzz")
 	    else:
 	    	print(i)
