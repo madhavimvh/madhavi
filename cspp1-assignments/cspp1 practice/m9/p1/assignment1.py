7f644cdb138f2238ccf37585ef9911ed0bede1f3
madhavimvh <mvhmadhavi@msitprogram.net>
2018-12-16 20:41:11 +0530

    " -> 0 of 9 passed. pylint: 8.00/10 "

--- cspp1-assignments/cspp1 practice/m9/p1/assignment1.py
+++ cspp1-assignments/cspp1 practice/m9/p1/assignment1.py
@@ -21,29 +21,30 @@ def main():
     '''
     Main function for the program
     '''
-    user_input = input()
-    if user_input:
-        data = user_input.split()
-        print(data)
-        secret_word = data[0]
-    else:
-        data = []
-        secret_word = ""
-    list1 = []
-    for j in range(1,len(data)):
-        list1.append(data[j])
-    print(list1)
-    # print(is_word_guessed(secret_word, list1))
     # user_input = input()
     # if user_input:
     #     data = user_input.split()
+    #     print(data)
     #     secret_word = data[0]
     # else:
     #     data = []
     #     secret_word = ""
     # list1 = []
-    # for j in range(1, len(data)):
-    #     list1.append(data[j][0])
+    # for j in range(1,len(data)):
+    #     list1.append(data[j])
+    # print(list1)
+    # print(is_word_guessed(secret_word, list1))
+    user_input = input()
+    if user_input:
+        data = user_input.split()
+        secret_word = data[0]
+    else:
+        data = []
+        secret_word = ""
+    list1 = []
+    for j in range(1, len(data)):
+        list1.append(data[j][0])
+    print(list1)
     # print(is_word_guessed(secret_word, list1))
 
 if __name__ == "__main__":
