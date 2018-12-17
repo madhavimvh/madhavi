ef9ac759f8c12b43d51e0c92e280a9b03b6acaef
madhavimvh <mvhmadhavi@msitprogram.net>
2018-12-17 13:37:46 +0530

    " -> 0 of 10 passed. pylint: 8.82/10 "

--- cspp1-assignments/cspp1 practice/m10/p1/assignment1.py
+++ cspp1-assignments/cspp1 practice/m10/p1/assignment1.py
@@ -14,6 +14,7 @@ def get_available_letters(letters_guessed):
     '''
     str1 = "abcdefghijklmnopqrstuvwxyz"
     list1 = str1.split()
+    strr = ""
     for i in list1:
     	if i not in letters_guessed:
     		strr += i
