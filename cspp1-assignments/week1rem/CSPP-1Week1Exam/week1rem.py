f7b29faebcee2129c611c21322d354c56d2601b7
madhavimvh <mvhmadhavi@msitprogram.net>
2018-12-29 11:51:43 +0530

    "cspp1_m10_a1_1: 2/3 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -1/10 "

--- cspp1-assignments/week1rem/CSPP-1Week1Exam/week1rem.py
+++ cspp1-assignments/week1rem/CSPP-1Week1Exam/week1rem.py
@@ -46,17 +46,62 @@ def check(postsrc, list1):
 	
 
 def background(html):
-	pass
+	data = html.replace("\n","").split("{")
+	global set1
+	set1 = set()
+	for each in data:
+		checkbackground(each)
+	set2 = []
+	for each in set1:
+		if len(each) <= 20:
+			set2.append(each.strip())
+	# print(set2)
+	sorted(set2)
+	for eachx in sorted(set2):
+		print(eachx)
+	print(len(set2))
+
+
+
+def checkbackground(each):
+	tag = "background-color:"
+	endtag = ";"
+	endtag2 = "}"
+	if tag in each:
+		if endtag in each:
+			# print(each)
+			# print("------------------------")
+			ind = each.find(tag)
+			posttag = each[ind + len(tag):]
+			# print(posttag)
+			end = posttag.find(endtag)
+			set1.add(posttag[:end])
+			if tag in posttag:
+				if endtag in posttag:
+					checkbackground(posttag)
+		elif endtag2 in each:
+			ind = each.find(tag)
+			posttag = each[ind + len(tag):]
+			# print(posttag)
+			end = posttag.find(endtag2)
+			set1.add(posttag[:end])
+			if tag in posttag:
+				if endtag2 in posttag:
+					checkbackground(posttag)
+
+
+
 def listtext(html):
 	pass
+	
 def main():
 	html = open("webpage5.html", errors='ignore').read()
 	# print(html)
-	# string = input()
-	# if string == "image":
-	image(html)
-	# elif string == "background":
-	background(html)
-	# elif string == "list":
-	listtext(html)
+	string = input()
+	if string == "image":
+		image(html)
+	elif string == "background":
+		background(html)
+	elif string == "list":
+		listtext(html)
 main()
