fc1f5ee12169c55be7609aefd91545bf37a2869f
madhavimvh <mvhmadhavi@msitprogram.net>
2019-01-03 00:22:27 +0530

    "cspp2_m10_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

---
+++ cspp1-assignments/CSPP2 Remedial Exam/Reservation.java
@@ -0,0 +1,37 @@
+/*
+  Reservation class, stores the person and room number in the Hotel
+*/
+
+public class Reservation{
+    //instance variables
+    private String name;
+    private int roomNumber;
+
+    //constructors, must supply the name, and optionally a room
+    public Reservation(String person){
+    	
+
+    }
+    public Reservation(String person, int room){
+    
+
+    }
+
+    //mutators, set the room number or name
+    public void setRoom(int newroom){
+    	
+    }
+
+    public void setName(String newname){
+    	
+    }
+
+    //accessors, return the room number or name
+    public int getRoom(){
+    	
+    }
+
+    public String getName(){
+    	
+    }
+}
---
+++ cspp1-assignments/CSPP2 Remedial Exam/reservation.py
@@ -0,0 +1,47 @@
+global dictx
+dictx = {}
+global count
+count = 0
+# def getroomno(count):
+#   count += 1
+#   if count not in dictx.values():
+#       print(count)
+#       return count
+#   else:
+#       getroomno(count)
+
+def reserve(name):
+    global count
+    global dictx
+    count += 1
+    if count not in dictx.values():
+        print(dictx.values())
+        dictx[name] = count
+    else:
+        reserve(name)
+    # print(dictx)
+
+def reserveN(name, roomno):
+    dictx[name] = int(roomno)
+    print(dictx)
+def display():
+    for each in dictx.keys():
+        print(str(each) + " " + str(dictx[each]))
+    # for key, value in sorted(dictx.items(), key = lambda kv: (-kv,kv[0])):
+    #     print("%s: %s" % (key, value))
+    for key, value in sorted(dictx.items(), key = lambda kv:(kv[1], kv[0])):
+        print("%s %s" %(key, value))
+def main():
+    # getroomno(count)
+    num = int(input())
+    string1 = []
+    for i in range(num):
+        string1 = input().split()
+        # print(string1)
+        if string1[0].strip() == "reserve":
+            reserve(string1[1])
+        elif string1[0].strip() == "reserveN":
+            reserveN(string1[1], string1[2])
+        elif string1[0].strip() == "print":
+            display()
+main()
