5592f65252c7d76854ae173e946d20dc6951263d
madhavimvh <mvhmadhavi@msitprogram.net>
2019-01-03 10:24:40 +0530

    "cspp2_m10_a1_1: 5/8 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 3/10 "

--- cspp1-assignments/CSPP2 Remedial Exam/reservation.py
+++ cspp1-assignments/CSPP2 Remedial Exam/reservation.py
@@ -69,6 +69,7 @@ def display():
         print("%s %s" %(key, value))
 def build(number):
     global variable
+    global str1
     str1 += "Added 2 more rooms" + "\n"
     variable += number
 def main():
