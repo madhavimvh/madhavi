0be793fc07eeb66b82725ad2763c19f0434d2e45
madhavimvh <mvhmadhavi@msitprogram.net>
2019-01-05 09:46:45 +0530

    "cspp1_m10_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -3/10 "

--- cspp1-assignments/CSPP1Week-2Exam testcases/listsoflists.py
+++ cspp1-assignments/CSPP1Week-2Exam testcases/listsoflists.py
@@ -1,8 +1,28 @@
 def main():
-	string = list(input())
-	list1 = []
+	global string
+	string = input().split()
 	print(string)
-
-
+	list1 = []
+	list2 = []
+	for i in range(len(string) - 1):
+		if string[i] == '[':
+			num = check(string[i + 1], i + 1)
+			a = num + 1
+			while True:
+				list2.append(string[a])
+				a = a + 1
+				if string[a] == ']':
+					list1.append(list2)
+					list2 = []
+					break
+	print(list1)
 
 main()
+def check(string, i):
+	if '[' in string:
+		a = i + 1
+		check(string[a], a)
+	else:
+		return i
+
+
