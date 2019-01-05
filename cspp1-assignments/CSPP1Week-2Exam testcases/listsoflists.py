6d31de4c2f52b1c5b302eab19b8a882ecf20c33a
madhavimvh <mvhmadhavi@msitprogram.net>
2019-01-05 16:00:02 +0530

    "cspp1_m10_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -3/10 "

--- cspp1-assignments/CSPP1Week-2Exam testcases/listsoflists.py
+++ cspp1-assignments/CSPP1Week-2Exam testcases/listsoflists.py
@@ -10,7 +10,7 @@ def listsfloat(string):
 				sum1 += float(each)
 			# print("sum" + str(sum1))
 		else:
-			check(each)
+			listsfloat(each)
 			count += 1
 	if "." in str(sum1):
 		sumstr = str(sum1).split(".")
@@ -46,6 +46,8 @@ def list_depth(list_of_lists):
             depth = 1
         else:
             depth = 1 + max([list_depth(l) for l in list_of_lists])
+            print(depth)
+            print([list_depth(l) for l in list_of_lists])
     else:
         depth = 0
     return depth
@@ -53,17 +55,7 @@ def main():
 	global string
 	string = eval(input())
 	listsfloat(string)
-	# try:
 	print(list_depth(string))
-	# except:
-		# print("cannot find depth")
-
-	# for each in string:
-	# 	if type(each) in [list]:
-	# 		for each1 in each: 
-	# 			print(type(each1))
-
-	
 
 main()
 
