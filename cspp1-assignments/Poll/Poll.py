3c218fb7f31d390b070ce86a7f8b40f9a58d6333
madhavimvh <mvhmadhavi@msitprogram.net>
2019-01-08 12:22:49 +0530

    "cspp2_m10_a1_1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -4/10 "

--- cspp1-assignments/Poll/Poll.py
+++ cspp1-assignments/Poll/Poll.py
@@ -1,9 +1,9 @@
 class Question:
 	list2 = []
-	optinvotes = {}
 	def __init__(self, question, options):
 		self.question = question
 		self.options = options
+		self.optinvotes = {}
 		print(options)
 		for opt in options:
 			self.optinvotes[opt] = 0
