e4aea3b7913bdad04a8f84c01081861cd92a740c
madhavimvh <mvhmadhavi@msitprogram.net>
2019-01-09 17:24:43 +0530

    "m19_a1: 5/13 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -2/10 "

--- cspp1-assignments/Assignment-1/Solution.py
+++ cspp1-assignments/Assignment-1/Solution.py
@@ -87,6 +87,9 @@ def main():
 							if string[0] == "" or string[1] == "" or string[2] == "" or string[3] == "" or string[4] == "":
 								k = "0"
 								raise Exception("Error! Malformed question")
+							elif int(string[2]) >= 4:
+								k = "0"
+								raise Exception("Error! Correct answer choice number is out of range for " + string[0])
 							else:
 								Ques = Question(string[0], string[1], string[2], string[3], string[4])
 								quiz.addquestion(Ques)
