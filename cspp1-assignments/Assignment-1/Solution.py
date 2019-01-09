3e9d3ca0085f78b6a6ae363233764b0c131fc615
madhavimvh <mvhmadhavi@msitprogram.net>
2019-01-09 15:46:39 +0530

    "m19_a1: 1/13 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -2/10 "

--- cspp1-assignments/Assignment-1/Solution.py
+++ cspp1-assignments/Assignment-1/Solution.py
@@ -34,8 +34,8 @@ class Quiz:
 			for eachopt in each.getoptions().split(","):
 				# print(each.getoptions().split(","))
 				str1 += eachopt + "\t"
-			str1 += "\n"
-			print(str1[:-1])
+			str1 = str1[:-1]
+			print(str1 + "\n")
 	def partoptions(self, list1):
 		for each in list1:
 			eachx = each.split()
