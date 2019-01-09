1401af8d5f48336b709f9ca11f8b320c052cf14b
madhavimvh <mvhmadhavi@msitprogram.net>
2019-01-09 13:31:47 +0530

    "m19_a1: 0/13 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -3/10 "

--- cspp1-assignments/Assignment-1/Solution.py
+++ cspp1-assignments/Assignment-1/Solution.py
@@ -16,9 +16,23 @@ class Quiz:
 def main():
 	quiz = Quiz()
 	n = input().split()
-	for i in range(int(n[1])):
-		string = input().split(":")
-		# print(string)
-		Ques = Question(string[0], string[1], string[2], string[3], string[4])
-		quiz.addquestion(Ques)
+	print(n)
+	if n[0] == "LOAD_QUESTIONS":
+		print("|----------------|")
+		print("| Load Questions |")
+		print("|----------------|")
+		print(n[1] + " are added to the quiz")
+		for i in range(int(n[1])):
+			string = input().split(":")
+			# print(string)
+			Ques = Question(string[0], string[1], string[2], string[3], string[4])
+			quiz.addquestion(Ques)
+	if n[0] == "START_QUIZ":
+		print("|------------|")
+		print("| Start Quiz |")
+		print("|------------|")
+	if n[0] == "SCORE_REPORT":
+		print("|--------------|")
+		print("| Score Report |")
+		print("|--------------|")
 main()
