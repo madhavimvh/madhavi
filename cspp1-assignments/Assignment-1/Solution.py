7cd6edfe997290299504c85f95d527d5ddd17425
madhavimvh <mvhmadhavi@msitprogram.net>
2019-01-09 13:14:35 +0530

    "m19_a1: 0/13 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

---
+++ cspp1-assignments/Assignment-1/Solution.py
@@ -0,0 +1,20 @@
+class Question:
+	def __init__(question, options, correctans, maxmarks, negmarks):
+		self.question = question
+		self.options = options
+		self.correctans = correctans
+		self.maxmarks = maxmarks
+		self.negmarks = negmarks
+class Quiz:
+	allquestions = []
+	def __init__():
+		pass
+	# def addquestion(Question):
+
+def main():
+	n = input().split()
+	for i in range(int(n[1])):
+		string = input().split()
+		print(string)
+		# quiz.addquestion()
+	main()
