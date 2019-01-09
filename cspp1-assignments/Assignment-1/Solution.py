c20485ff9dabef9e8075bb621186bbad464c6c22
madhavimvh <mvhmadhavi@msitprogram.net>
2019-01-09 14:30:50 +0530

    "m19_a1: 0/13 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -2/10 "

--- cspp1-assignments/Assignment-1/Solution.py
+++ cspp1-assignments/Assignment-1/Solution.py
@@ -43,16 +43,17 @@ class Quiz:
 		for each in self.allquestions:
 			print(each.getquestext())
 			for part_ch in self.partchoice:
+				print(self.partchoice)
 				print(part_ch)
 				if each.getcorrectans() == part_ch:
 					print(" Correct Answer! - Marks Awarded: " + each.getmaxmarks())
 					self.totscore += int(each.getmaxmarks())
 					del self.partchoice[self.partchoice.index(part_ch)]
-					print(self.partchoice)
 					break
 				else:
 					print("Wrong Answer! - Penalty: " + each.getnegmarks())
 					self.totscore -= int(each.getnegmarks())
+					del self.partchoice[self.partchoice.index(part_ch)]
 					break
 		print("Total Score: " + str(self.totscore))
 
