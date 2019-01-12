96e6d154b3d1ea251e1f3e1e3b28e877caf0334d
madhavimvh <mvhmadhavi@msitprogram.net>
2019-01-12 10:18:46 +0530

    "cspp1_m10_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -1/10 "

--- cspp1-assignments/CSPP1-Week3Exam/week3exam.py
+++ cspp1-assignments/CSPP1-Week3Exam/week3exam.py
@@ -14,23 +14,18 @@ def main():
 		n = int(input())
 		for i in range(n):
 			string = input().split("|")
-		try:
-			# print(string[4])
-			if string[4].isdigit():
-				if string[0] not in adict:
-					adict[string[0]] = 0
-					adict1[string[0]] = int(string[4])
-				else:
-					adict1[string[0]] += int(string[4])
-				if string[2] == string[3]:
-					adict[string[0]] += int(string[4])
-				else:
-					adict[string[0]] -= int(string[4])
-			# print(adict)
-			# print(adict1)
-			display(adict, adict1)
-		except ValueError:
-			print("sjdflk")
+			if string[0] not in adict:
+				adict[string[0]] = 0
+				adict1[string[0]] = int(string[4])
+			else:
+				adict1[string[0]] += int(string[4])
+			if string[2] == string[3]:
+				adict[string[0]] += int(string[4])
+			else:
+				adict[string[0]] -= int(string[4])
+		# print(adict)
+		# print(adict1)
+		display(adict, adict1)
 
 
 
