ce17d58f9457b3c04e2b11f14e9ad951d237474c
madhavimvh <mvhmadhavi@msitprogram.net>
2019-01-12 09:43:48 +0530

    "cspp1_m10_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

---
+++ cspp1-assignments/CSPP1-Week3Exam/week3exam.py
@@ -0,0 +1,29 @@
+def display(adict, adict1):
+	score = 0
+	for key1 in adict:
+		for key2 in adict1:
+			if key1 == key2:
+				# print(adict[key1], adict1[key2])
+				score = int((adict[key1]/adict1[key2])*100)
+				print(key1 + ": " + str(float(score)))
+def main():
+	adict = {}
+	adict1 = {}
+	n = int(input())
+	for i in range(n):
+		string = input().split("|")
+		if string[0] not in adict:
+			adict[string[0]] = 0
+			adict1[string[0]] = int(string[4])
+		else:
+			adict1[string[0]] += int(string[4])
+		if string[2] == string[3]:
+			adict[string[0]] += int(string[4])
+		else:
+			adict[string[0]] -= int(string[4])
+	# print(adict)
+	# print(adict1)
+	display(adict, adict1)
+
+
+main()
