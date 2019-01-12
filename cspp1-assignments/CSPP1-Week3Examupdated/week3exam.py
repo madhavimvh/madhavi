43d7f9649198b9d6808b0840d751a50bd6e74b52
madhavimvh <mvhmadhavi@msitprogram.net>
2019-01-12 12:55:12 +0530

    "cspp1_m10_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -1/10 "

--- cspp1-assignments/CSPP1-Week3Examupdated/week3exam.py
+++ cspp1-assignments/CSPP1-Week3Examupdated/week3exam.py
@@ -44,42 +44,78 @@ def listtodict(s):
 		orddict1[each[0]] = each[1]
 	# print(orddict1)
 	return orddict1
+def displaynew(adict, adict1):
+	adictx = {}
+	for each in sorted(adict):
+		for eachx in sorted(adict1):
+			total = 0
+			if each == eachx:
+				adictx = adict[each]
+				print(adictx)
+				for each1 in adictx:
+					total += adictx[each1]
+				score = total/adict1[eachx]
+				print(eachx)
+				print(total)
+				print(score)
 
 def main():
 		adict = {}
 		adict1 = {}
 		ques = {}
 		n = int(input())
+		try:
+			for i in range(n):
+				string = input().split("|")
+				int(string[4])
+				if string[0] not in adict:
+					ques = {}
+					adict[string[0]] = 0
+					adict1[string[0]] = int(string[4])
+				else:
+					adict1[string[0]] += int(string[4])
+				if string[2] == string[3]:
+					adict[string[0]] += int(string[4])
+				else:
+					adict[string[0]] -= int(string[4])
+			# print(adict)
+			# print(adict1)
+			display(adict, adict1)
+		except ValueError:
+			print("Invalid Points")
 		# try:
 		# 	for i in range(n):
 		# 		string = input().split("|")
 		# 		int(string[4])
 		# 		if string[0] not in adict:
 		# 			ques = {}
-		# 			adict[string[0]] = 0
-		# 			adict1[string[0]] = int(string[4])
+		# 			if string[2] == string[3]:
+		# 				ques[string[1]] = int(string[4])
+		# 				adict[string[0]] = ques
+		# 				adict1[string[0]] = int(string[4])
+		# 			else:
+		# 				ques[string[1]] = "-" + string[4]
+		# 				adict[string[0]] = ques
+		# 				adict1[string[0]] = int(string[4])
+
+		# 			# print(adict)
 		# 		else:
 		# 			adict1[string[0]] += int(string[4])
-		# 		if string[2] == string[3]:
-		# 			adict[string[0]] += int(string[4])
-		# 		else:
-		# 			adict[string[0]] -= int(string[4])
-		# 	# print(adict)
-		# 	# print(adict1)
-		# 	display(adict, adict1)
-		# except ValueError:
-		# 	print("Invalid Points")
-		try:
-			for i in range(n):
-				string = input.split("|")
-				int(string[4])
-				if string[0] not in adict:
-					ques = {}
-					ques[string[1]] = int(string[4])
-					adict[string[0]] = ques
-				else:
-					ques[string[1]] = int(string[4])
-				print(ques)
+		# 			for each in adict:
+		# 				if each == string[0]:
+		# 					if string[2] == string[3]:
+		# 						ques = adict[string[0]]
+		# 						ques[string[1]] = int(string[4])
+		# 					else:
+		# 						ques = adict[string[0]]
+		# 						ques[string[1]] = "-" + string[4]
+
+
+
+			# print(adict)
+			# print(adict1)
+			# displaynew(adict, adict1)
+
 		except ValueError:
 			print("Invalid Points")
 main()
