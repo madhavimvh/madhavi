ecb40f142270a55f574722af5fc3b3a4ba708234
madhavimvh <mvhmadhavi@msitprogram.net>
2019-01-23 22:18:51 +0530

    "final_exam_a1_1: 3/14 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -3/10 "

--- cspp1-assignments/todolist/Assignment-1/todolist.py
+++ cspp1-assignments/todolist/Assignment-1/todolist.py
@@ -6,19 +6,21 @@ class Task:
 		self.title = title
 		self.status = status
 		self.time = time
-		try:
-			if title == "":
-				raise Exception("Title not provided")
-			# else:
-				# self.title = title
-			elif int(time) < 0:
-				raise Exception("Invalid timeToComplete " + time)
-			# else:
-				# self.time = time
-			elif status != "todo" or status != "done":
-				raise Exception("Invalid status " + status)	
-		except Exception as e:
-			print(e)
+		# try:
+		if self.title == "":
+			raise Exception("Title not provided")
+			return
+		# else:
+			# self.title = title
+		elif int(self.time) < 0:
+			raise Exception("Invalid timeToComplete " + self.time)
+			return
+		# else:
+			# self.time = time
+		elif self.status != "todo" and self.status != "done":
+			raise Exception("Invalid status " + self.status)	
+		# except Exception as e:
+			# print(e)
 	def display(self):
 		if self.imp == "y":
 			self.imp = "Important"
@@ -33,7 +35,10 @@ class Task:
 
 def main():
 	string = input().split(",")
-	task = Task(string[1], string[2], string[3], string[4], string[5], string[6])
-	if string[0] == "task":
-		task.display()
+	try:
+		if string[0] == "task":
+			task = Task(string[1], string[2], string[3], string[4], string[5], string[6])
+			task.display()
+	except Exception as e:
+			print(e)
 main()
