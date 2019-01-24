7e612224b60f4af1d21a44183bf73184fae99e4a
madhavimvh <mvhmadhavi@msitprogram.net>
2019-01-24 22:16:20 +0530

    "cspp1_m10_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -2/10 "

--- cspp1-assignments/CSPP1-Week3Exam/sol2.py
+++ cspp1-assignments/CSPP1-Week3Exam/sol2.py
@@ -14,46 +14,50 @@ def display(adict1):
 def main():
 	adict = {}
 	n = int(input())
-	for i in range(n):
-		string = input().split("|")
-		if string[0] not in adict:
-			# print(string[0])
-			ques = {}
-			if string[2] == string[3]:
-				ques[string[1]] = int(string[4])
+	try:
+		for i in range(n):
+			string = input().split("|")
+			int(string[4])
+			if string[0] not in adict:
+				# print(string[0])
+				ques = {}
+				if string[2] == string[3]:
+					ques[string[1]] = int(string[4])
+				else:
+					ques[string[1]] = -1*int(string[4])
+				# print(ques)
+				adict[string[0]] = ques
 			else:
-				ques[string[1]] = -1*int(string[4])
-			# print(ques)
-			adict[string[0]] = ques
-		else:
-			for each in adict:
-				if each == string[0]:
-					ques = adict[each]
-					if string[2] == string[3]:
-						ques[string[1]] = int(string[4])
-					else:
-						ques[string[1]] = -1*int(string[4])
+				for each in adict:
+					if each == string[0]:
+						ques = adict[each]
+						if string[2] == string[3]:
+							ques[string[1]] = int(string[4])
+						else:
+							ques[string[1]] = -1*int(string[4])
 
 
 
-	# print(adict)
-	adict1 = {}
-	for each in adict:
-		maxm = 0
-		tot = 0
-		ques = adict[each]
-		for each1 in ques:
-			maxm += ques[each1]
-			tot += abs(ques[each1])
-		if each not in adict1:
-			adict1[each] = [maxm]
-			adict1[each].append(tot)
-		else:
-			for each2 in adict1:
-				if each2 == each:
-					adict1[each] = [maxm]
-					adict1[each].append(tot)
+		# print(adict)
+		adict1 = {}
+		for each in adict:
+			maxm = 0
+			tot = 0
+			ques = adict[each]
+			for each1 in ques:
+				maxm += ques[each1]
+				tot += abs(ques[each1])
+			if each not in adict1:
+				adict1[each] = [maxm]
+				adict1[each].append(tot)
+			else:
+				for each2 in adict1:
+					if each2 == each:
+						adict1[each] = [maxm]
+						adict1[each].append(tot)
 
-	# print(adict1)
-	display(adict1)
+		# print(adict1)
+		display(adict1)
+	except ValueError:
+		print("Invalid points")
 main()
