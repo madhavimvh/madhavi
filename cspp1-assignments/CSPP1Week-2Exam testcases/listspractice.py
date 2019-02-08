8dd69af76dabbfda1a52a542c5dc87b773775dd3
madhavimvh <mvhmadhavi@msitprogram.net>
2019-02-08 17:10:14 +0530

    "cspp1_m10_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -3/10 "

--- cspp1-assignments/CSPP1Week-2Exam testcases/listspractice.py
+++ cspp1-assignments/CSPP1Week-2Exam testcases/listspractice.py
@@ -1,11 +1,14 @@
 def  lists(input1):
 	summ = 0
 	for each in input1:
+		# if type(each) not in list:
+
+
 		for num in each:
 			summ += num
-	print(summ)  
+			print(summ)  
 def main():
 	input1 = eval(input())
-	print(input1)
+	# print(input1)
 	lists(input1)
 main()
