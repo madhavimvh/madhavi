55dea0199b0f89063265b3c08bbc5626ec4d7fa0
madhavimvh <mvhmadhavi@msitprogram.net>
2019-08-23 22:47:12 +0530

    "cspp1_m10_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -10/10 "

--- cspp1-assignments/CSPP1Week-2Exam testcases/listsoflists.py
+++ cspp1-assignments/CSPP1Week-2Exam testcases/listsoflists.py
@@ -1,73 +1,78 @@
-global size 
-size = 0
-global count
-count = 0
-global sum1
-sum1 = 0
-def listsfloat(string):
-	global sum1
-	global count
-	# size = 0
-	# count = 0
-	# sum1 = 0
-	for each in string:
-		# if type(each) not in list:
-		if isinstance(each, list) == False:
-			# if type(each) not in str:
-			if isinstance(each, str) == False:
-				# print(each)
-				sum1 += float(each)
-			# print("sum" + str(sum1))
-		else:
-			count += 1
-			listsfloat(each)
-			# print(each)
-	print(sum1)
-	# if "." in str(sum1):
-	# 	sumstr = str(sum1).split(".")
-	# 	if sumstr[1] != '0':
-	# 		print(sum1)
-	# 	else:
-	# 		print(int(sum1))
-	# else:
-	# 	print(sum1)
-	# print(count)
+# global size 
+# size = 0
+# global count
+# count = 0
+# global sum1
+# sum1 = 0
+# def listsfloat(string):
+# 	global sum1
+# 	global count
+# 	# size = 0
+# 	# count = 0
+# 	# sum1 = 0
+# 	for each in string:
+# 		# if type(each) not in list:
+# 		if isinstance(each, list) == False:
+# 			# if type(each) not in str:
+# 			if isinstance(each, str) == False:
+# 				# print(each)
+# 				sum1 += float(each)
+# 			# print("sum" + str(sum1))
+# 		else:
+# 			count += 1
+# 			listsfloat(each)
+# 			# print(each)
+# 	print(sum1)
+# 	# if "." in str(sum1):
+# 	# 	sumstr = str(sum1).split(".")
+# 	# 	if sumstr[1] != '0':
+# 	# 		print(sum1)
+# 	# 	else:
+# 	# 		print(int(sum1))
+# 	# else:
+# 	# 	print(sum1)
+# 	# print(count)
 
-def check(each):
-	global sum1
-	global count
-	for each1 in each:
-		if type(each1) not in [list]:
-			if type(each1) not in [str]:
-				sum1 += float(each1)
-		else:
-			# print("list")
-			check(each1)
-			count += 1
-	# print(sum1)
-# def liststr(string):
-# def depth(l):
-#     if isinstance(l, list):
-#         return 1 + max(depth(item) for item in l)
+# def check(each):
+# 	global sum1
+# 	global count
+# 	for each1 in each:
+# 		if type(each1) not in [list]:
+# 			if type(each1) not in [str]:
+# 				sum1 += float(each1)
+# 		else:
+# 			# print("list")
+# 			check(each1)
+# 			count += 1
+# 	# print(sum1)
+# # def liststr(string):
+# # def depth(l):
+# #     if isinstance(l, list):
+# #         return 1 + max(depth(item) for item in l)
+# #     else:
+# #         return 0
+# def list_depth(list_of_lists):
+#     if isinstance(list_of_lists, list):
+#         if(len(list_of_lists) == 0):
+#             depth = 1
+#         else:
+#             depth = 1 + max([list_depth(l) for l in list_of_lists])
+#             # print(depth)
+#             print([list_depth(l) for l in list_of_lists])
 #     else:
-#         return 0
-def list_depth(list_of_lists):
-    if isinstance(list_of_lists, list):
-        if(len(list_of_lists) == 0):
-            depth = 1
-        else:
-            depth = 1 + max([list_depth(l) for l in list_of_lists])
-            # print(depth)
-            print([list_depth(l) for l in list_of_lists])
-    else:
-        depth = 0
-    return depth
+#         depth = 0
+#     return depth
+# def main():
+# 	global string
+# 	string = eval(input())
+# 	listsfloat(string)
+# 	# print(list_depth(string))
+# main()
+
+
 def main():
 	global string
 	string = eval(input())
-	listsfloat(string)
-	# print(list_depth(string))
-
+	print(string)
 main()
-
 
